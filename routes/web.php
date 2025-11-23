897f5579cb963c261fcd494a3461ff8ea3f30ca1
amirithm <amirithm01@gmail.com>
2025-11-23 05:58:56 +0330

    implements oauth in routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\GithubController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('/oauth')->group(function(){
+    Route::prefix('/github')->group(function(){
+        Route::get('/redirect',[GithubController::class,'redirect']);
+        Route::get('/callback',[GithubController::class,'callback']);
+    });
+});
+
